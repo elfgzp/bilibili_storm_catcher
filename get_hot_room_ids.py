7ad7c4790565d5a58b8dba0bb2514c4d6bff5f05
elf_gzp <741424975@qq.com>
2017-11-11 01:42:16 +0800

    fix

--- get_hot_room_ids.py
+++ get_hot_room_ids.py
@@ -5,7 +5,7 @@ import json
 def get_room_ids(page):
     url = 'http://api.live.bilibili.com/area/liveList'
     response = requests.get(url, params={'area': 'all', 'order': 'online', 'page': page})
-    data = json.loads(response.content)
+    data = json.loads(response.text)
     room_ids = []
     if data['code'] == 0:
         room_ids = [each['roomid'] for each in data['data']]
